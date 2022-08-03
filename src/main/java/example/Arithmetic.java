879133702efbe3006c16057a9487f4410ec03c21
sdineen <dineen701@gmail.com>
2022-08-03 08:46:38 +0100

    Initial commit

---
+++ src/main/java/example/Arithmetic.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Arithmetic {
+
+	public static int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+
+	public static long factorial(int i) {
+		// TODO Auto-generated method stub
+		long result = 1;
+		for (; i > 1; i--) {
+			result *= i;
+		}
+		return result;
+	}
+
+}
---
+++ src/test/java/example/ArithmeticTest.java
@@ -0,0 +1,18 @@
+package example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ArithmeticTest {
+	
+	@Test
+	public void addTest() {
+		assertEquals(4, Arithmetic.add(2,2));
+	}
+	
+	@Test
+	public void factorialTest() {
+		assertEquals(120, Arithmetic.factorial(5));
+	}
+}
