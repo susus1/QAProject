33d377b02e8c5ec173a5c951d8f7bc262d681e8f
sdineen <dineen701@gmail.com>
2022-08-03 11:50:49 +0100

    a

--- src/main/java/examples/exceptions/Runner.java
+++ src/main/java/examples/exceptions/Runner.java
@@ -1,6 +1,7 @@
 package examples.exceptions;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,14 +11,14 @@ public class Runner {
 	public static void main(String[] args) {
 
 		try {
-			Path path = Path.of("C:\\Users\\Owner\\Documents\\Java");
-			String s = Files.readString(null); //checked exception
+			Path path = Path.of("C:/Users/Owner/Documents/file.txt");
+			String s = Files.readString(path); //checked exception
 			int i = Integer.parseInt(s); //unchecked exception
 			System.out.println(i);
 		} catch (NumberFormatException e) {
-			System.out.println(e.getMessage());
+			System.out.println("not a number");
 		} catch (IOException e) {
-
+			System.out.println("File unavailable");
 		}
 
 		finally {
