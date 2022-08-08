1b9b6904c36502c9e19d66171ba3c52c580c6c50
sdineen <dineen701@gmail.com>
2022-08-08 08:51:21 +0100

    cr

---
+++ src/main/java/design/patterns/chainofresponsibility/ChainPatternDemo.java
@@ -0,0 +1,32 @@
+package design.patterns.chainofresponsibility;
+
+/**
+ * Create different types of loggers. Assign them error levels and set next logger in each logger. 
+ * Next logger in each logger represents the part of the chain.
+ * @author Owner
+ *
+ */
+public class ChainPatternDemo {
+
+	private static AbstractLogger getChainOfLoggers() {
+
+		AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+		AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+		AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+		errorLogger.setNextLogger(fileLogger);
+		fileLogger.setNextLogger(consoleLogger);
+
+		return errorLogger;
+	}
+
+	public static void main(String[] args) {
+		AbstractLogger loggerChain = getChainOfLoggers();
+
+		loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+		loggerChain.logMessage(AbstractLogger.DEBUG, "This is an debug level information.");
+
+		loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+	}
+}
