a2e7e050d2fe03bf093be02d79274fd88738d808
sdineen <dineen701@gmail.com>
2022-08-09 16:26:24 +0100

    Update MapMusicTrackRepository.java

--- src/main/java/examples/collections/MapMusicTrackRepository.java
+++ src/main/java/examples/collections/MapMusicTrackRepository.java
@@ -12,31 +12,34 @@ public class MapMusicTrackRepository implements MusicTrackRepository {
 	public boolean add(MusicTrack musicTrack) {
 		// call the map's put method, passing in the id and the music track object
 		// return true if the method returns null
-		return false;
+		MusicTrack replaces = map.put(musicTrack.getId(), musicTrack);
+		return replaces == null;
 	}
 
 	@Override
 	public MusicTrack selectById(long id) {
 		// call the map's get method, passing in the id
-		return null;
+		
+		return map.get(id);
 	}
 
 	@Override
 	public Collection<MusicTrack> selectAll() {
 		// return the values() method
-		return null;
+		return map.values();
 	}
 
 	@Override
 	public boolean update(MusicTrack musicTrack) {
 		// call the map's replace method, passing in the id of the music track as the first parameter 
-		return false;
+		MusicTrack replaces = map.replace(musicTrack.getId(), musicTrack);
+		return replaces != null;
 	}
 
 	@Override
 	public boolean delete(MusicTrack musicTrack) {
 		// call the map's remove method, passing in the id
-		return false;
+		return map.remove(musicTrack.getId(), musicTrack);
 	}
 
 }
