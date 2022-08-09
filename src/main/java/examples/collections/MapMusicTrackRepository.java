a6c2d26ce7fcec9335dc963ee43de0f913e3ed2b
sdineen <dineen701@gmail.com>
2022-08-09 16:09:11 +0100

    map

--- src/main/java/examples/collections/MapMusicTrackRepository.java
+++ src/main/java/examples/collections/MapMusicTrackRepository.java
@@ -1,36 +1,41 @@
 package examples.collections;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapMusicTrackRepository implements MusicTrackRepository {
+	
+	private Map<Long, MusicTrack> map = new HashMap<>();
 
 	@Override
 	public boolean add(MusicTrack musicTrack) {
-		// TODO Auto-generated method stub
+		// call the map's put method, passing in the id and the music track object
+		// return true if the method returns null
 		return false;
 	}
 
 	@Override
 	public MusicTrack selectById(long id) {
-		// TODO Auto-generated method stub
+		// call the map's get method, passing in the id
 		return null;
 	}
 
 	@Override
 	public Collection<MusicTrack> selectAll() {
-		// TODO Auto-generated method stub
+		// return the values() method
 		return null;
 	}
 
 	@Override
 	public boolean update(MusicTrack musicTrack) {
-		// TODO Auto-generated method stub
+		// call the map's replace method, passing in the id of the music track as the first parameter 
 		return false;
 	}
 
 	@Override
 	public boolean delete(MusicTrack musicTrack) {
-		// TODO Auto-generated method stub
+		// call the map's remove method, passing in the id
 		return false;
 	}
 
--- src/main/java/examples/collections/Runner.java
+++ src/main/java/examples/collections/Runner.java
@@ -11,7 +11,7 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
-		MusicTrackRepository repository = new ListMusicTrackRepository();
+		MusicTrackRepository repository = new SetMusicTrackRepository();
 		
 		MusicTrack track1 = new MusicTrack(1L,"Expresso Love", "Making Movies", "Dire Staits");
 		MusicTrack track2 = new MusicTrack(2L, "Bat out of hell", "Bat out of Hell", "Meat Loaf");
