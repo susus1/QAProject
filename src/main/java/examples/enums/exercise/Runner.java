ec090ed7b95cb9725cf850a00e5b17f2459c73af
sdineen <dineen701@gmail.com>
2022-08-10 09:19:57 +0100

    w

--- src/main/java/examples/enums/exercise/Runner.java
+++ src/main/java/examples/enums/exercise/Runner.java
@@ -3,9 +3,9 @@ package examples.enums.exercise;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DayOfWeek d = DayOfWeek.MONDAY;
-		System.out.println(d.daysToWeekend());
+
+//		DayOfWeek d = DayOfWeek.MONDAY;
+//		System.out.println(d.daysToWeekend());
 		
 		//java.time.DayOfWeek d;
 	}
