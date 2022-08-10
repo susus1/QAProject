1fe55ff24d77c179309787a39fbd10e7595616a1
sdineen <dineen701@gmail.com>
2022-08-10 12:31:49 +0100

    Update Runner.java

--- src/main/java/examples/enums/exercise/Runner.java
+++ src/main/java/examples/enums/exercise/Runner.java
@@ -4,11 +4,20 @@ public class Runner {
 
 	public static void main(String[] args) {
 
-		DayOfWeek d = DayOfWeek.MONDAY;
-		for (DayOfWeek day : d.values()) {
-			System.out.printf("%d   %s %d days to the weekend%n",day.getNumber(),
-					day.toString(), day.daysToWeekend());
+//		DayOfWeek d = DayOfWeek.MONDAY;
+//		for (DayOfWeek day : d.values()) {
+//			System.out.printf("%d   %s %d days to the weekend%n",day.getNumber(),
+//					day.toString(), day.daysToWeekend());
+//		}
+		
+		
+		java.time.DayOfWeek d2 = java.time.DayOfWeek.WEDNESDAY;
+		
+		for (java.time.DayOfWeek dayOfWeek : java.time.DayOfWeek.values()) {
+			System.out.printf("%d %s%n",dayOfWeek.getValue(), dayOfWeek);
 		}
+		
+		
 
 	}
 
