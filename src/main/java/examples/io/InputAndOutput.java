4a9b00a218e0ad45d79f4479d872f6e17755cda2
sdineen <dineen701@gmail.com>
2022-08-11 10:45:12 +0100

    io

--- src/main/java/examples/io/InputAndOutput.java
+++ src/main/java/examples/io/InputAndOutput.java
@@ -1,11 +1,29 @@
 package examples.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class InputAndOutput {
+	
+    public void bufferedCopy()  {
+        try(
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/textFile.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/resources/newTextFile.txt"))
+        ) {
+            String line;
+            while((line = br.readLine()) != null) {
+                bw.write(line + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 
 	public void copy(String inFile, String outFile) {
 		try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/" + inFile);
@@ -23,6 +41,7 @@ public class InputAndOutput {
 	
 	public static void main(String[] args) {
 		InputAndOutput io = new InputAndOutput();
-		io.copy("photo.jpg", "copy.jpg");
+		io.bufferedCopy();
+//		io.copy("photo.jpg", "copy.jpg");
 	}
 }
